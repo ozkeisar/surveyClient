a7a806752fba1ed0c47a65511bbd8dd14a2975e9
strengthCaesar <caesar7670@live.com>
2018-09-27 22:11:14 +0300

    create user info page / register page

--- components/homePages/userInfo/userInfo.js
+++ components/homePages/userInfo/userInfo.js
@@ -27,22 +27,25 @@ class userInfo extends React.Component {
     }
 
     async getStoredData(){
-        // try {
-            // const value = await AsyncStorage.getItem('userInfo');
+        try {
+            // await AsyncStorage.removeItem('userInfo');
+            const value = await AsyncStorage.getItem('userInfo');
             // console.log('value ',value);
-            // if (value !== null) {
-                // this.setState({ isData: true });
+            if (value !== null) {
+
                 // this.state = { isData: true }
-                // this.userInfo = value;
-                // console.log(value);
-            // }
-        // } catch (error) {
+                this.userInfo = JSON.parse(value);
+                console.log(this.userInfo);
+                console.log(this.userInfo.fullName);
+                this.setState({ isData: true });
+            }
+        } catch (error) {
             // Error retrieving data
-        // }
+        }
     }
 
     async saveData(){
-        if(this.isPhoneNumber(this.userInfo.phone)) {
+        if(this.isPhoneNumber(this.userInfo.phoneNumber)) {
 
             this.userInfo._id = await this.register();
             console.log('user info 2',this.userInfo);
@@ -59,28 +62,39 @@ class userInfo extends React.Component {
     }
 
     async register() {
+        let formBody = [];
 
         let url = 'http://192.168.43.176:3000/register';
 
-        req={
-            userInfo:{
-                name:this.userInfo.fullName
-            },
-            phoneNumber:this.userInfo.phone,
-            location:this.userInfo.location
-        }
-        console.log('user req 3',req);
+        let encodedKey = encodeURIComponent("userInfo");
+        let encodedValue = encodeURIComponent(this.userInfo.fullName);
+        formBody.push(encodedKey + "=" + encodedValue);
+        encodedKey = encodeURIComponent("location");
+        encodedValue = encodeURIComponent(this.userInfo.location);
+        formBody.push(encodedKey + "=" + encodedValue);
+        encodedKey = encodeURIComponent("phoneNumber");
+        encodedValue = encodeURIComponent(this.userInfo.phoneNumber);
+        formBody.push(encodedKey + "=" + encodedValue);
 
-        let _id = await fetch(url, {
+        formBody = formBody.join("&");
+
+        // console.log('user req 3',JSON.stringify(req));
+
+        let res = await fetch(url, {
             method: 'POST',
             headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(req),
-        });
+                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                AcceptLanguage: "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7",
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
 
-        return _id;
+            },
+            body: formBody,
+        });
+        let h = JSON.parse(await res.text());
+        // console.log(await res.text());
+        console.log(h._id);
+
+        return h._id;
     }
 
     isPhoneNumber(num){
@@ -101,7 +115,7 @@ class userInfo extends React.Component {
                     }/>
                     <FormLabel>{"*מס פלאפון"}</FormLabel>
                     <FormInput onChangeText={(text) => {
-                        this.userInfo.phone = text;
+                        this.userInfo.phoneNumber = text;
                     }
                     }/>
                     <FormLabel>{"עיר/ישוב"}</FormLabel>
@@ -133,6 +147,23 @@ class userInfo extends React.Component {
         }else {
             return (
                 <View>
+                    <View style={ styles.line}>
+                        <Text>{"שם:"}</Text>
+                        <Text>{this.userInfo.fullName}</Text>
+                    </View>
+                    <View style={ styles.line}>
+                        <Text>{"כתובת:"}</Text>
+                        <Text>{this.userInfo.location}</Text>
+                    </View>
+                    <View style={ styles.line}>
+                        <Text>{"פלאפון:"}</Text>
+                        <Text>{this.userInfo.phoneNumber}</Text>
+                    </View>
+                    <View style={ styles.line}>
+                        <Text>{":id"}</Text>
+                        <Text>{this.userInfo._id}</Text>
+                    </View>
+
 
                     <Button
                         style={ styles.bottomView}
@@ -160,6 +191,9 @@ const styles = StyleSheet.create({
         // position: 'absolute',
         bottom: 0
     },
+    line:{
+        flexDirection: 'row'
+    }
 });
 
 
