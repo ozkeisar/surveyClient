92eba6844f9f68d3011e15d436f0469381b68ddf
strengthCaesar <caesar7670@live.com>
2018-09-28 11:13:09 +0300

    party details page

--- components/homePages/partyDetailsPage/partyDetails.js
+++ components/homePages/partyDetailsPage/partyDetails.js
@@ -1,21 +1,32 @@
 
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button,StyleSheet } from 'react-native';
 
 class partyDetails extends React.Component {
     static navigationOptions = {
-        title: 'partyDetails',
+        title: 'פרטי מפלגה',
     };
 
 
     render() {
         const param = this.props.navigation.getParam('partyInfo', 'NO-Details')
-        console.log('ll',param);
         return (
+        <View>
+            <Text style={styles.titleText}> {param.name}</Text>
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>partyDetails Screen {param.name}</Text>
+                <Text>{param._id}</Text>
             </View>
+        </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    titleText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
+
 export default partyDetails;
