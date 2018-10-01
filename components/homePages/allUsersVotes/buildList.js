07990f9fca5a753f9ffd4ce7f0d44d3b1bc9c853
strengthCaesar <caesar7670@live.com>
2018-10-02 00:05:23 +0300

    add avatar to party

--- components/homePages/allUsersVotes/buildList.js
+++ components/homePages/allUsersVotes/buildList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FlatList, ActivityIndicator, Text, View,ScrollView ,Dimensions ,I18nManager,TouchableOpacity} from 'react-native';
+import { FormLabel, FormInput, Avatar} from 'react-native-elements'
 import GetPartiesList from './http/getPartiesList';
 import VoteButton from './vote/button';
 import { withNavigation } from 'react-navigation';
 
-// I18nManager.forceRTL(true);
+I18nManager.forceRTL(true);
 var width = Dimensions.get('window').width;
+const borderWidth = 0;
 
 class ListParties extends React.Component {
 
@@ -29,7 +31,7 @@ class ListParties extends React.Component {
 
     getStyle=(index)=>{
         let height = 80;
-        return index != 0?{flexDirection: 'row', height:height, }:{flexDirection: 'row',flex: 1, height:height, backgroundColor:"#adff2f"};
+        return index != 0?{width: width,flexDirection: 'row', height:height, }:{flexDirection: 'row',flex: 1, height:height, backgroundColor:"#adff2f"};
     }
 
 
@@ -41,31 +43,45 @@ class ListParties extends React.Component {
 
 
 
-    render(){
+    render() {
 
-        if(this.state.isLoading){
-            return(
+        if (this.state.isLoading) {
+            return (
                 <View style={{flex: 1, padding: 20}}>
-                    <GetPartiesList getData={this.getData} />
+                    <GetPartiesList getData={this.getData}/>
                     <ActivityIndicator/>
                 </View>
             )
         }
 
 
-        return(
+        return (
             <View>
-               <ScrollView>
+                <ScrollView>
                     <FlatList
                         data={this.state.dataSource.parties}
                         renderItem={
-                            ({item , index}) =>
-                                <View style={this.getStyle(index)} >
-                                    <TouchableOpacity  style={{width:width*.75,justifyContent: 'center'}} onPress={()=>{this.openDetailsPage(item)}}>
+                            ({item, index}) =>
+                                <View style={this.getStyle(index)}>
+                                    <View style={{padding:5, borderWidth:borderWidth, justifyContent: 'center', width:width*.15}}>
+                                        <Avatar
+                                            size="large"
+                                            rounded
+                                            source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"}}
+                                            onPress={() => console.log("Works! ",width)}
+                                            activeOpacity={0.7}
+                                        />
+                                    </View>
+
+                                    <TouchableOpacity style={{borderWidth:borderWidth,justifyContent: 'center', width:width*.6}}
+                                                      onPress={() => {
+                                                          this.openDetailsPage(item)
+                                                      }}>
+
                                         <Text>{item.name}</Text>
                                         <Text>{'מנדטים:'}{item.mandates}</Text>
                                     </TouchableOpacity>
-                                    <View  style={{flex: 1,justifyContent: 'center'}}>
+                                    <View style={{padding:5,borderWidth:borderWidth, justifyContent:  'center', width:width*.2}}>
                                         <VoteButton partyId={item._id}/>
                                     </View>
 
